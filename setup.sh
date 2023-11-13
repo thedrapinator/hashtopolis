f53b3f6f75f4155f9a70f0c453f8dc9891687c70
thedrapinator <85844648+thedrapinator@users.noreply.github.com>
2023-11-13 16:45:35 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,11 @@
 sudo apt update
 sudo apt install docker docker-compose-v2 python3-pip -y
 
+#Make directory
+mkdir ~/hashtopolis
+cd ~/hashtopolis
 
+#Get config files
 wget https://raw.githubusercontent.com/hashtopolis/server/master/docker-compose.yml
 wget https://raw.githubusercontent.com/hashtopolis/server/master/env.example -O .env
 #Edit hashtopolis env file with users and passwords
@@ -12,13 +16,10 @@ wget https://raw.githubusercontent.com/hashtopolis/server/master/env.example -O 
 sudo docker compose up
 
 #Agent python3 packages
+sudo apt install python3-pip
 pip3 install requests psutil
 
 #Download zip from agents tab and run
 python3 hashtopolis.zip
 
 #Enter URL to API and the Vocher from the agents tab. The agent automatically starts and is ready to go
-
-
-
-
