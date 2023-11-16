208700bc25e1cb0e74d3cba53782c956d8a9ccc5
thedrapinator <85844648+thedrapinator@users.noreply.github.com>
2023-11-16 12:43:49 -0500

    Update setup.sh
    reorder installs

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@
 sudo apt update
 sudo apt install docker docker-compose-v2 python3-pip git -y
 
+#Agent python3 packages
+pip3 install requests psutil
+
 #Make directory
 mkdir ~/hashtopolis
 cd ~/hashtopolis
@@ -18,21 +21,17 @@ cp env.example .env
 #Edit hashtopolis env file with users and passwords
 
 #need sudo??? I did at this time
-sudo docker compose up
+sudo docker compose up -d
 
-#Agent python3 packages
-sudo apt install python3-pip
-pip3 install requests psutil
+
 
 #Download zip from agents tab (http://127.0.0.1:8080/agents.php) and run
-python3 hashtopolis.zip
+#python3 hashtopolis.zip
 
 #Enter URL to API and the Vocher from the agents tab. The agent automatically starts and is ready to go
 #Can automate this with flags!!! - https://github.com/hashtopolis/agent-python
 
-
 #Possible SSL Solution
 #https://luiscoutinh.medium.com/how-to-fix-unsecure-connection-in-local-environment-with-docker-and-docker-compose-d58c2f89754a
-
 
 
